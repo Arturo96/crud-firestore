edf258fb25c81cc0d4c0ad6cc1daaf6e777ff63d
Carlos Vargas <carlosospino13@gmail.com>
2020-07-28 15:14:04 -0500

    CRUD terminado

--- js/index.js
+++ js/index.js
@@ -1,33 +1,36 @@
 const d = document;
-
+let id = "";
 const db = firebase.firestore();
 
-const formTask = d.getElementById('formTask');
+const formTask = d.getElementById("formTask");
 
-const agregarTarea = (title, desc) => db.collection('tareas').doc().set({
-        nombre: title,
-        descripcion: desc
-    });
+const agregarTarea = (title, desc) =>
+	db.collection("tareas").doc().set({
+		nombre: title,
+		descripcion: desc
+	});
 
-const borrarTarea = id => db.collection('tareas').doc(id).delete();
+const borrarTarea = id => db.collection("tareas").doc(id).delete();
 
-const editarTarea = id => {
-    
-}
+const editarTarea = (id, updatedTask) => db.collection("tareas").doc(id).update(updatedTask);
 
-const listarTareas = () => db.collection('tareas').get();
+const onChangeTasks = callback => db.collection("tareas").onSnapshot(callback);
 
-const cargarTareas = async() => {
-    const divTareas = d.getElementById('tareas');
-    const querySnapshot = await listarTareas();
-    
-    divTareas.innerHTML = "";
+const listarTareas = () => db.collection("tareas").get();
 
-    querySnapshot.forEach(doc => {
-        const {id} = doc;
-        const {nombre, descripcion} = doc.data();
-        
-        divTareas.innerHTML += `
+const listarTareaPorId = id => db.collection("tareas").doc(id).get();
+
+const cargarTareas = async () => {
+	const divTareas = d.getElementById("tareas");
+	const querySnapshot = await listarTareas();
+
+	divTareas.innerHTML = "";
+
+	querySnapshot.forEach(doc => {
+		const { id } = doc;
+		let { nombre, descripcion } = doc.data();
+
+		divTareas.innerHTML += `
         <tr>
             <td>${nombre}</td>
             <td>${descripcion}</td>
@@ -36,42 +39,62 @@ const cargarTareas = async() => {
                 <button data-id=${id} class="btn btn-danger btn-delete">Borrar</button>
             </td>
         </tr>
-        `
-    })
+        `;
+	});
 
-    const btnDeletes = d.querySelectorAll('.btn-delete');
+	const btnDeletes = d.querySelectorAll(".btn-delete");
 
-    btnDeletes.forEach(btn => {
-        btn.addEventListener('click', async(e) => {
-            await borrarTarea(e.target.dataset.id);
-            cargarTareas();
-        })
-    })
+	btnDeletes.forEach(btn => {
+		btn.addEventListener("click", async e => {
+			await borrarTarea(e.target.dataset.id);
+			cargarTareas();
+		});
+	});
 
-    const btnUpdates = d.querySelectorAll('.btn-update');
+	const btnUpdates = d.querySelectorAll(".btn-update");
 
-    btnUpdates.forEach(btn => {
-        btn.addEventListener('click', async(e) => {
-            console.log(e.target.dataset.id);
-            // cargarTareas();
-        })
-    })
-}
+	btnUpdates.forEach(btn => {
+		btn.addEventListener("click", async e => {
+			id = e.target.dataset.id;
 
-addEventListener('DOMContentLoaded', async(e) => {
-    cargarTareas();
-})
+			const doc = await listarTareaPorId(id);
+			let { nombre, descripcion } = doc.data();
 
-formTask.addEventListener('submit', async(e) => {
-    e.preventDefault();
+			d.getElementById("taskTitle").value = nombre;
+			d.getElementById("taskDesc").value = descripcion;
 
-    const taskTitle = d.getElementById('taskTitle');
-    const taskDesc = d.getElementById('taskDesc');
+			d.getElementById("btnAddTask").textContent = "Actualizar";
 
-    await agregarTarea(taskTitle.value, taskDesc.value);
+			
+		});
+	});
+};
 
-    e.target.reset();
-    taskTitle.focus();
+addEventListener("DOMContentLoaded", async e => {
+	cargarTareas();
+});
 
-    cargarTareas();
-})
+formTask.addEventListener("submit", async e => {
+	e.preventDefault();
+
+	const taskTitle = d.getElementById("taskTitle");
+	const taskDesc = d.getElementById("taskDesc");
+
+	if (d.getElementById("btnAddTask").textContent === "Agregar") {
+		await agregarTarea(taskTitle.value, taskDesc.value);
+	} else {
+		const updatedTask = {
+			nombre: taskTitle.value,
+			descripcion: taskDesc.value
+		};
+		console.log(updatedTask);
+		await editarTarea(id, updatedTask);
+		d.getElementById("btnAddTask").textContent = 'Agregar'
+		cargarTareas();
+	}
+
+	e.target.reset();
+	taskTitle.focus();
+
+	cargarTareas();
+});
